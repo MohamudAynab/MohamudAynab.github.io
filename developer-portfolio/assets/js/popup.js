fd29e7db811bc4111a7263ac3addc6861d7ef213
Mohamud Aynab <mfaynab88@gmail.com>
2023-12-14 09:52:20 -0500

    testing popup function

--- developer-portfolio/assets/js/popup.js
+++ developer-portfolio/assets/js/popup.js
@@ -1,15 +1,7 @@
 window.onload = function () {
 	// Show the pop-up message using a custom dialog
-	var popupMessage =
-		"Whoopsie-daisy! 🙈 We're currently giving our site a makeover. Swing by again soon for a fancier experience! Thanks for your patience! 🚀";
 
-	// Display the custom confirm dialog
-	var popup = confirm(popupMessage);
-	p;
-	// If the user clicks "OK," no further action needed
-	if (!popup) {
-		// If the user clicks "Cancel," close the pop-up window
-
-		window.close();
-	}
+	alert(
+		'Thank you for your visit, an updated version of this site in this in the works. Deploying midnight 2024. 🚀 ',
+	);
 };
